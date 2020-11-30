5e203b1395ffe13c9a1baaebc443a86f3138ed98
deepak <deepak.mehta@cardinaltsinc.net>
2020-11-30 20:42:48 +0530

    task1 for functional component

---
+++ src/component/MovieList.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import {Table} from "react-bootstrap";
+const MovieList = ()=> {
+    const [movie, setMovie] = useState("");
+    const [rating, setRating] = useState("");
+    const [finalMovieList, setFinalList] = useState([]);
+    
+    
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        if(movie){
+            setFinalList(finalMovieList.concat({'movie': movie, 'rating':rating}));
+        }
+        else {
+            alert(`No movie provided.`)
+        }
+        // alert(`Submitting Name ${movie}`)
+        // alert(`Submitting rating ${rating}`);
+        
+    }
+
+    let finalMovieArr="";
+    if(finalMovieList.length>0){
+        console.log('in if finalMovieList---',finalMovieList);
+        finalMovieArr = [finalMovieList.map((obj,i)=>{
+            return (
+                <tr key={i}>
+                    <td>{obj.movie}</td>
+                    <td>{obj.rating}</td>
+                </tr>
+            )
+        })]
+    }else{
+        console.log('in else finalMovieList---',finalMovieList);
+        finalMovieArr = [<tr key="0">
+            <td colSpan="2">No Records</td>
+        </tr>]
+    }
+
+    console.log("finalMovieArr---", finalMovieArr);
+return (
+
+<div style={{textAlign:"center", padding:"10px 232px"}}>
+    <form onSubmit={handleSubmit}>
+    <h1 style={{textAlign:'center'}}>Movie List</h1>
+        <div className="movieDiv">
+            <span className="spanLayout">
+                <label>Movie Name:</label> <input type="text" id="movieName" name="movieName" onChange={e => setMovie(e.target.value)} /> 
+            </span>
+            <span className="spanLayout">
+                <label>Rating:</label> <input type="number" min="1" max="5" id="rating" name="rating" onChange={e => setRating(e.target.value)}/> 
+            </span>
+            <span className="spanLayout"><input type="submit" value="Submit" /></span>
+        </div>
+    </form>
+    
+    <div className="details-box" style={{position:'relative'}}>
+        <div className="gridlist">
+        <Table size="sm">
+            <thead>
+                <tr>
+                    <th style={{width:'160px'}} >Movie</th>
+                    <th style={{width:'160px'}} >Rating</th>
+                </tr>
+            </thead>
+            <tbody>
+                {finalMovieArr}
+            </tbody>
+        </Table>
+    </div>
+    </div>
+</div>
+    )
+}
+
+export default MovieList
