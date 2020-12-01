874c7c1767726821124cd6584633ac39673ac17f
deepak <deepak.mehta@cardinaltsinc.net>
2020-12-01 17:50:55 +0530

    Updated the code as per the feedback

--- src/component/MovieList.jsx
+++ src/component/MovieList.jsx
@@ -1,70 +1,75 @@
 import React, { useState } from "react";
-import {Table} from "react-bootstrap";
-const MovieList = ()=> {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Button } from "react-bootstrap";
+const MovieList = () => {
     const [movie, setMovie] = useState("");
     const [rating, setRating] = useState("");
     const [finalMovieList, setFinalList] = useState([]);
-    
-    
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-        if(movie){
-            setFinalList(finalMovieList.concat({'movie': movie, 'rating':rating}));
+    const [errorMsg, setErrorMsg] = useState(false);
+
+
+    const handleSubmit = () => {
+        if (movie && rating) {
+            const movieListObj = finalMovieList.concat({ 'movie': movie, 'rating': rating });
+            setFinalList(movieListObj);
+            setErrorMsg('');
         }
         else {
-            alert(`No movie provided.`)
+            if (!movie)
+                setErrorMsg("No movie name provided.");
+            else {
+                setErrorMsg("No rating is provided.");
+            }
         }
     }
 
-    let finalMovieArr="";
-    if(finalMovieList.length>0){
-        finalMovieArr = [finalMovieList.map((obj,i)=>{
-            return (
-                <tr key={i}>
-                    <td>{obj.movie}</td>
-                    <td>{obj.rating}</td>
-                </tr>
-            )
-        })]
-    }else{
-        finalMovieArr = [<tr key="0">
-            <td colSpan="2">No Records</td>
-        </tr>]
+    const closeErrorAlert = () => {
+        setErrorMsg('');
     }
 
-    
-return (
 
-<div style={{textAlign:"center", padding:"10px 232px"}}>
-    <form onSubmit={handleSubmit}>
-    <h1 style={{textAlign:'center'}}>Movie List</h1>
-        <div className="movieDiv">
-            <span className="spanLayout">
-                <label>Movie Name:</label> <input type="text" id="movieName" name="movieName" onChange={e => setMovie(e.target.value)} /> 
-            </span>
-            <span className="spanLayout">
-                <label>Rating:</label> <input type="number" min="1" max="5" id="rating" name="rating" onChange={e => setRating(e.target.value)}/> 
-            </span>
-            <span className="spanLayout"><input type="submit" value="Submit" /></span>
+    return (
+
+        <div className="main-div">
+            {errorMsg ? (<div className="alert alert-danger alert-dismissible" ><a href="#" class="close" data-dismiss="alert" aria-label="close" onClick={() => closeErrorAlert()}>&times;</a><strong>{errorMsg}</strong></div>) : (null)}
+            <form>
+                <h1 className="h1-ele">Movie List</h1>
+                <div className="movieDiv">
+                    <span className="spanLayout">
+                        <label>Movie Name:</label> <input type="text" id="movieName" name="movieName" onChange={e => setMovie(e.target.value)} />
+                    </span>
+                    <span className="spanLayout">
+                        <label>Rating:</label> <input type="number" min="1" max="5" id="rating" name="rating" onChange={e => setRating(e.target.value)} />
+                    </span>
+                    <span className="spanLayout"><Button id="addMovie" onClick={() => handleSubmit()}>Submit</Button></span>
+                </div>
+            </form>
+
+            <div className="details-box">
+                <div className="gridlist">
+                    <Table size="sm">
+                        <thead>
+                            <tr>
+                                <th className="th-ele">Movie</th>
+                                <th className="th-ele">Rating</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {finalMovieList.map((obj, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td>{obj.movie}</td>
+                                        <td>{obj.rating}</td>
+                                    </tr>
+                                )
+                            })
+                            }
+
+                        </tbody>
+                    </Table>
+                </div>
+            </div>
         </div>
-    </form>
-    
-    <div className="details-box" style={{position:'relative'}}>
-        <div className="gridlist">
-        <Table size="sm">
-            <thead>
-                <tr>
-                    <th style={{width:'160px'}} >Movie</th>
-                    <th style={{width:'160px'}} >Rating</th>
-                </tr>
-            </thead>
-            <tbody>
-                {finalMovieArr}
-            </tbody>
-        </Table>
-    </div>
-    </div>
-</div>
     )
 }
 
